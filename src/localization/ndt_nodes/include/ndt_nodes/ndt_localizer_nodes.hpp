2230533a9a5d4c83377f7e4371f35da548ee3c6c
Igor Bogoslavskyi <igor.bogoslavskyi@apex.ai>
2020-07-17 15:45:10 +0200

    wip: update ndt localization node to new base node

--- src/localization/ndt_nodes/include/ndt_nodes/ndt_localizer_nodes.hpp
+++ src/localization/ndt_nodes/include/ndt_nodes/ndt_localizer_nodes.hpp
@@ -49,67 +49,49 @@ using PoseInitializer_ = localization_common::BestEffortInitializer;
 /// P2D NDT localizer node. Currently uses the hard coded optimizer and pose initializers.
 /// \tparam OptimizerT Hard coded for Newton optimizer. TODO(yunus.caliskan): Make Configurable
 /// \tparam PoseInitializerT Hard coded for Best effort. TODO(yunus.caliskan): Make Configurable
-template<typename OptimizerT = Optimizer_, typename PoseInitializerT = PoseInitializer_>
+template<typename LocalizerT, typename MapT, typename PoseInitializerT>
 class NDT_NODES_PUBLIC P2DNDTLocalizerNode
   : public localization_nodes::RelativeLocalizerNode<
     sensor_msgs::msg::PointCloud2,
-    sensor_msgs::msg::PointCloud2,
-    ndt::P2DNDTLocalizer<OptimizerT>,
+    MapT,
+    LocalizerT,
     PoseInitializerT>
 {
 public:
-  using Localizer = ndt::P2DNDTLocalizer<OptimizerT>;
-  using RegistrationSummary = typename Localizer::RegistrationSummary;
-  using LocalizerBasePtr = std::unique_ptr<localization_common::RelativeLocalizerBase<
-        sensor_msgs::msg::PointCloud2,
-        sensor_msgs::msg::PointCloud2,
-        RegistrationSummary>>;
   using ParentT = localization_nodes::RelativeLocalizerNode<
     sensor_msgs::msg::PointCloud2,
-    sensor_msgs::msg::PointCloud2,
-    Localizer,
+    MapT,
+    LocalizerT,
     PoseInitializerT>;
-  using PoseWithCovarianceStamped = typename Localizer::PoseWithCovarianceStamped;
-  using Transform = typename Localizer::Transform;
+  using RegistrationSummary = typename ParentT::RegistrationSummary;
 
   using EigTranslation = Eigen::Vector3d;
   using EigRotation = Eigen::Quaterniond;
   static constexpr auto EPS = std::numeric_limits<ndt::Real>::epsilon();
 
+protected:
   /// Constructor
   /// \param node_name node name
   /// \param name_space node namespace
   /// \param pose_initializer pose initializer to use.
   P2DNDTLocalizerNode(
     const std::string & node_name,
-    const std::string & name_space,
-    const PoseInitializerT & pose_initializer)
-  : ParentT(node_name, name_space, pose_initializer),
+    const rclcpp::NodeOptions & options,
+    LocalizerT && localizer,
+    PoseInitializerT && pose_initializer)
+  : ParentT{
+      node_name,
+      options,
+      std::move(localizer),
+      MapT{get_map_config()},
+      std::move(pose_initializer)},
     m_predict_translation_threshold{
       this->declare_parameter("predict_pose_threshold.translation").template get<double>()},
     m_predict_rotation_threshold{
       this->declare_parameter("predict_pose_threshold.rotation").template get<double>()}
   {
-    auto get_point_param = [this](const std::string & config_name_prefix) {
-        perception::filters::voxel_grid::PointXYZ point;
-        point.x = static_cast<float32_t>(this->declare_parameter(config_name_prefix + ".x").
-          template get<float32_t>());
-        point.y = static_cast<float32_t>(this->declare_parameter(config_name_prefix + ".y").
-          template get<float32_t>());
-        point.z = static_cast<float32_t>(this->declare_parameter(config_name_prefix + ".z").
-          template get<float32_t>());
-        return point;
-      };
-    // Fetch map configuration
-    const auto capacity = static_cast<std::size_t>(
-      this->declare_parameter("localizer.map.capacity").template get<std::size_t>());
-    const perception::filters::voxel_grid::Config map_config{get_point_param(
-        "localizer.map.min_point"), get_point_param("localizer.map.max_point"),
-      get_point_param("localizer.map.voxel_size"), capacity};
-
     // Fetch localizer configuration
     ndt::P2DNDTLocalizerConfig localizer_config{
-      map_config,
       static_cast<uint32_t>(this->declare_parameter("localizer.scan.capacity").
       template get<uint32_t>()),
       std::chrono::milliseconds(static_cast<uint64_t>(
@@ -119,39 +101,38 @@ public:
     const auto outlier_ratio{this->declare_parameter(
         "localizer.optimization.outlier_ratio").template get<float64_t>()};
 
-    common::optimization::OptimizationOptions optimizer_options{
-      static_cast<uint64_t>(
-        this->declare_parameter("localizer.optimizer.max_iterations").template get<uint64_t>()),
-      this->declare_parameter("localizer.optimizer.score_tolerance").template get<float64_t>(),
-      this->declare_parameter(
-        "localizer.optimizer.parameter_tolerance").template get<float64_t>(),
-      this->declare_parameter("localizer.optimizer.gradient_tolerance").template get<float64_t>()
-    };
+    // common::optimization::OptimizationOptions optimizer_options{
+    //   static_cast<uint64_t>(
+    //     this->declare_parameter("localizer.optimizer.max_iterations").template get<uint64_t>()),
+    //   this->declare_parameter("localizer.optimizer.score_tolerance").template get<float64_t>(),
+    //   this->declare_parameter(
+    //     "localizer.optimizer.parameter_tolerance").template get<float64_t>(),
+    //   this->declare_parameter("localizer.optimizer.gradient_tolerance").template get<float64_t>()
+    // };
 
     // Construct and set the localizer.
-    const float32_t step_max{static_cast<float32_t>(this->declare_parameter(
-        "localizer.optimizer.line_search.step_max").
-      template get<float64_t>())};
-    const float32_t step_min{static_cast<float32_t>(this->declare_parameter(
-        "localizer.optimizer.line_search.step_min").
-      template get<float64_t>())};
+    // const float32_t step_max{static_cast<float32_t>(this->declare_parameter(
+    //     "localizer.optimizer.line_search.step_max").
+    //   template get<float64_t>())};
+    // const float32_t step_min{static_cast<float32_t>(this->declare_parameter(
+    //     "localizer.optimizer.line_search.step_min").
+    //   template get<float64_t>())};
     // TODO(igor): make the line search configurable.
-    LocalizerBasePtr localizer_ptr = std::make_unique<Localizer>(
-      localizer_config,
-      OptimizerT{
-            common::optimization::MoreThuenteLineSearch{
-              step_max, step_min,
-              common::optimization::MoreThuenteLineSearch::OptimizationDirection::kMaximization},
-            optimizer_options
-          },
-      outlier_ratio);
-    this->set_localizer(std::move(localizer_ptr));
+    // LocalizerBasePtr localizer_ptr = std::make_unique<LocalizerT>(
+    //   localizer_config,
+    //   OptimizerT{
+    //         common::optimization::MoreThuenteLineSearch{
+    //           step_max, step_min,
+    //           common::optimization::MoreThuenteLineSearch::OptimizationDirection::kMaximization},
+    //         optimizer_options
+    //       },
+    //   outlier_ratio);
   }
 
-protected:
   bool validate_output(
     const RegistrationSummary & summary,
-    const PoseWithCovarianceStamped & pose, const Transform & guess) override
+    const geometry_msgs::msg::PoseWithCovarianceStamped & pose,
+    const geometry_msgs::msg::TransformStamped & guess) override
   {
     bool ret = true;
     switch (summary.optimization_summary().termination_type()) {
@@ -177,9 +158,32 @@ protected:
   }
 
 private:
+  perception::filters::voxel_grid::Config get_map_config()
+  {
+    auto get_point_param = [this](const std::string & config_name_prefix) {
+        perception::filters::voxel_grid::PointXYZ point;
+        point.x = static_cast<float32_t>(this->declare_parameter(config_name_prefix + ".x").
+          template get<float32_t>());
+        point.y = static_cast<float32_t>(this->declare_parameter(config_name_prefix + ".y").
+          template get<float32_t>());
+        point.z = static_cast<float32_t>(this->declare_parameter(config_name_prefix + ".z").
+          template get<float32_t>());
+        return point;
+      };
+
+    return perception::filters::voxel_grid::Config{
+      get_point_param("localizer.map.min_point"),
+      get_point_param("localizer.map.max_point"),
+      get_point_param("localizer.map.voxel_size"),
+      static_cast<std::size_t>(
+        this->declare_parameter("localizer.map.capacity").template get<std::size_t>())};
+  }
+
+
   virtual bool on_non_convergence(
     const RegistrationSummary &,
-    const PoseWithCovarianceStamped &, const Transform &)
+    const geometry_msgs::msg::PoseWithCovarianceStamped &,
+    const geometry_msgs::msg::TransformStamped &)
   {
     // In practice, it's hard to come up with a perfect termination criterion for ndt
     // optimization and even non-convergence may be a decent effort in localizing the
@@ -192,12 +196,16 @@ private:
   /// \param pose NDT pose estimate.
   /// \param guess Initial guess for the localizer.
   /// \return True if translation estimate is valid.
-  virtual bool translation_valid(const PoseWithCovarianceStamped & pose, const Transform guess)
+  virtual bool translation_valid(
+    const geometry_msgs::msg::PoseWithCovarianceStamped & pose,
+    const geometry_msgs::msg::TransformStamped guess)
   {
-    EigTranslation pose_translation{pose.pose.pose.position.x,
+    EigTranslation pose_translation{
+      pose.pose.pose.position.x,
       pose.pose.pose.position.y,
       pose.pose.pose.position.z};
-    EigTranslation guess_translation{guess.transform.translation.x,
+    EigTranslation guess_translation{
+      guess.transform.translation.x,
       guess.transform.translation.y,
       guess.transform.translation.z};
     EigTranslation diff = pose_translation - guess_translation;
@@ -208,14 +216,18 @@ private:
   /// \param pose NDT pose estimate.
   /// \param guess Initial guess for the localizer.
   /// \return True if rotation estimate is valid.
-  virtual bool rotation_valid(const PoseWithCovarianceStamped & pose, const Transform guess)
+  virtual bool rotation_valid(
+    const geometry_msgs::msg::PoseWithCovarianceStamped & pose,
+    const geometry_msgs::msg::TransformStamped guess)
   {
-    EigRotation pose_rotation{pose.pose.pose.orientation.w,
+    EigRotation pose_rotation{
+      pose.pose.pose.orientation.w,
       pose.pose.pose.orientation.x,
       pose.pose.pose.orientation.y,
       pose.pose.pose.orientation.z
     };
-    EigRotation guess_rotation{guess.transform.rotation.x,
+    EigRotation guess_rotation{
+      guess.transform.rotation.x,
       guess.transform.rotation.x,
       guess.transform.rotation.y,
       guess.transform.rotation.z
