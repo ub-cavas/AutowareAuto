77ef058bfc81b42e9285631f2f881af7452d74ad
Igor Bogoslavskyi <igor.bogoslavskyi@apex.ai>
2020-07-31 18:07:55 +0200

    wip: decouple ndt mapping node from localizer node

--- src/mapping/ndt_mapping_nodes/include/ndt_mapping_nodes/ndt_mapping_nodes.hpp
+++ src/mapping/ndt_mapping_nodes/include/ndt_mapping_nodes/ndt_mapping_nodes.hpp
@@ -22,8 +22,10 @@
 #include <localization_common/initialization.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <point_cloud_mapping/point_cloud_mapper.hpp>
+#include <time_utils/time_utils.hpp>
 #include <point_cloud_mapping/policies.hpp>
 #include <ndt/ndt_localizer.hpp>
+#include <helper_functions/message_adapters.hpp>
 #include <optimization/newtons_method_optimizer.hpp>
 #include <localization_nodes/localization_node.hpp>
 #include <optimization/line_search/more_thuente_line_search.hpp>
@@ -40,6 +42,8 @@ namespace mapping
 {
 namespace ndt_mapping_nodes
 {
+using common::helper_functions::message_field_adapters::get_frame_id;
+using common::helper_functions::message_field_adapters::get_stamp;
 
 // TODO(yunus.caliskan) remove the hard-coded optimizer set up and make it fully configurable
 using Optimizer = common::optimization::NewtonsMethodOptimizer<
@@ -53,14 +57,8 @@ using Mapper = point_cloud_mapping::PointCloudMapper<
   Localizer::RegistrationSummary,
   point_cloud_mapping::CapacityTrigger,
   point_cloud_mapping::TimeStampPrefixGenerator>;
-using RelativeLocalizerNode = localization::localization_nodes::RelativeLocalizerNode<
-  sensor_msgs::msg::PointCloud2,
-  sensor_msgs::msg::PointCloud2,
-  Mapper,
-  PoseInitializer>;
 
-
-class NDT_MAPPING_NODES_PUBLIC P2DNDTVoxelMapperNode : public RelativeLocalizerNode
+class NDT_MAPPING_NODES_PUBLIC P2DNDTVoxelMapperNode : public rclcpp::Node
 {
 public:
   using PoseWithCovarianceStamped = geometry_msgs::msg::PoseWithCovarianceStamped;
@@ -69,7 +67,7 @@ public:
 
   // TODO(yunus.caliskan): Probably set the pose initializer explicitly.
   explicit P2DNDTVoxelMapperNode(const rclcpp::NodeOptions & options)
-  : RelativeLocalizerNode{"ndt_mapper_node", options, PoseInitializer{}} {init();}
+  : rclcpp::Node{"ndt_mapper_node", options} {init();}
 
 private:
   void init()
@@ -141,10 +139,10 @@ private:
     const auto & map_frame_id = this->declare_parameter("map.frame_id").template get<std::string>();
     VoxelMap map{parse_grid_config("map"), map_frame_id};
 
-    this->set_localizer(std::make_unique<Mapper>(
-        this->declare_parameter("file_name_prefix").template get<std::string>(),
-        std::move(map), std::move(localizer_ptr), map_frame_id
-    ));
+    m_mapper_ptr = std::make_unique<Mapper>(
+      this->declare_parameter("file_name_prefix").template get<std::string>(),
+      std::move(map), std::move(localizer_ptr), map_frame_id
+    );
 
     if (this->declare_parameter("publish_map_increment").template get<bool8_t>()) {
       m_increment_publisher = this->template create_publisher<sensor_msgs::msg::PointCloud2>(
@@ -155,10 +153,56 @@ private:
     }
   }
 
+  /// Callback that registers each received observation and outputs the result.
+  /// \param msg_ptr Pointer to the observation message.
+  void observation_callback(typename sensor_msgs::msg::PointCloud2::ConstSharedPtr msg_ptr)
+  {
+    try {
+      const auto observation_time = ::time_utils::from_message(get_stamp(*msg_ptr));
+      const auto & observation_frame = get_frame_id(*msg_ptr);
+      const auto & map_frame = m_mapper_ptr->map_frame_id();
+
+      ////////////////////////////////////////////////////////
+      // TODO(yunus.caliskan): remove in #425
+      if (m_use_hack && !m_hack_initialized) {
+        check_and_execute_hack(get_stamp(*msg_ptr));
+      }
+      /////////////////////////////////////////////////////////
+
+      const auto initial_guess =
+        m_pose_initializer.guess(m_tf_buffer, observation_time, map_frame, observation_frame);
+
+      m_hack_initialized = true;    // Only after a successful lookup, disable the hack.
+
+      PoseWithCovarianceStamped pose_out;
+      const auto summary = m_mapper_ptr->on_new_measurement(*msg_ptr, initial_guess, pose_out);
+      if (validate_output(summary, pose_out, initial_guess)) {
+        m_pose_publisher->publish(pose_out);
+        // This is to be used when no state estimator or alternative source of
+        // localization is available.
+        if (m_tf_publisher) {
+          publish_tf(pose_out);
+        }
+        publish_map_increment(summary);
+      } else {
+        RCLCPP_WARN(
+          get_logger(),
+          "Relative localizer has an invalid pose estimate. The result is ignored.");
+      }
+    } catch (...) {
+      // TODO(mitsudome-r) remove this hack in #458
+      if (m_tf_publisher) {
+        republish_tf(get_stamp(*msg_ptr));
+      }
+      // on_bad_registration(std::current_exception());
+    }
+  }
+
+
   bool8_t validate_output(
     const MapperSummary & summary,
     const PoseWithCovarianceStamped & pose,
-    const Transform & guess) override
+    const Transform & guess)
   {
     bool8_t ret = true;
     if (summary.localizer_summary) {
@@ -235,16 +279,81 @@ private:
       std::numeric_limits<autoware::common::types::float64_t>::epsilon());
   }
 
-  void handle_registration_summary(const MapperSummary & summary) override
+  void publish_map_increment(const MapperSummary & summary)
   {
     if (m_increment_publisher && summary.map_increment) {
       m_increment_publisher->publish(*summary.map_increment);
     }
   }
 
-  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_increment_publisher{nullptr};
+  // TODO(igor): remove then this functionality is consolidated with the relative localizer node.
+  /// Publish the pose message as a transform.
+  void publish_tf(const PoseWithCovarianceStamped & pose_msg)
+  {
+    const auto & pose = pose_msg.pose.pose;
+    tf2::Quaternion rotation{pose.orientation.x, pose.orientation.y, pose.orientation.z,
+      pose.orientation.w};
+    tf2::Vector3 translation{pose.position.x, pose.position.y, pose.position.z};
+    const tf2::Transform map_base_link_transform{rotation, translation};
+
+    geometry_msgs::msg::TransformStamped odom_tf;
+    try {
+      odom_tf = m_tf_buffer.lookupTransform("odom", "base_link",
+          time_utils::from_message(pose_msg.header.stamp));
+    } catch (const tf2::ExtrapolationException &) {
+      odom_tf = m_tf_buffer.lookupTransform("odom", "base_link", tf2::TimePointZero);
+    }
+    tf2::Quaternion odom_rotation{odom_tf.transform.rotation.x,
+      odom_tf.transform.rotation.y, odom_tf.transform.rotation.z, odom_tf.transform.rotation.w};
+    tf2::Vector3 odom_translation{odom_tf.transform.translation.x, odom_tf.transform.translation.y,
+      odom_tf.transform.translation.z};
+    const tf2::Transform odom_base_link_transform{odom_rotation, odom_translation};
+
+    const auto map_odom_tf = map_base_link_transform * odom_base_link_transform.inverse();
+
+    tf2_msgs::msg::TFMessage tf_message;
+    geometry_msgs::msg::TransformStamped tf_stamped;
+    tf_stamped.header.stamp = pose_msg.header.stamp;
+    tf_stamped.header.frame_id = m_mapper_ptr->map_frame_id();
+    tf_stamped.child_frame_id = "odom";
+    const auto & tf_trans = map_odom_tf.getOrigin();
+    const auto & tf_rot = map_odom_tf.getRotation();
+    tf_stamped.transform.translation.set__x(tf_trans.x()).set__y(tf_trans.y()).
+    set__z(tf_trans.z());
+    tf_stamped.transform.rotation.set__x(tf_rot.x()).set__y(tf_rot.y()).set__z(tf_rot.z()).
+    set__w(tf_rot.w());
+    tf_message.transforms.push_back(tf_stamped);
+    m_tf_publisher->publish(tf_message);
+  }
+
+  // TODO(mitsudome-r) remove this hack in #458
+  /// Publish the pose message as a transform.
+  void republish_tf(builtin_interfaces::msg::Time stamp)
+  {
+    auto map_odom_tf = m_tf_buffer.lookupTransform(m_mapper_ptr->map_frame_id(), "odom",
+        tf2::TimePointZero);
+    map_odom_tf.header.stamp = stamp;
+    tf2_msgs::msg::TFMessage tf_message;
+    tf_message.transforms.push_back(map_odom_tf);
+    m_tf_publisher->publish(tf_message);
+  }
+
+  tf2::BufferCore m_tf_buffer;
+  tf2_ros::TransformListener m_tf_listener;
+
+  PoseInitializer m_pose_initializer{};
+  std::unique_ptr<Mapper> m_mapper_ptr{};
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_increment_publisher{};
   autoware::common::types::float64_t m_predict_translation_threshold{};
   autoware::common::types::float64_t m_predict_rotation_threshold{};
+
+  rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr m_pose_publisher{};
+  rclcpp::Publisher<tf2_msgs::msg::TFMessage>::SharedPtr m_tf_publisher{};
+
+  // TODO(yunus.caliskan): Remove hack variables below in #425
+  bool m_use_hack{false};
+  bool m_hack_initialized{false};
+  geometry_msgs::msg::TransformStamped m_init_hack_transform;
 };
 
 }  // namespace ndt_mapping_nodes
