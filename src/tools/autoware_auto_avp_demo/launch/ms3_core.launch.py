946de751269e3af3d161438ea92433bf9dc414d3
Frederik Beaujean <Frederik.Beaujean@apex.ai>
2020-10-01 20:03:45 +0200

    Start websocket as Node instead of ExecuteProcess

--- src/tools/autoware_auto_avp_demo/launch/ms3_core.launch.py
+++ src/tools/autoware_auto_avp_demo/launch/ms3_core.launch.py
@@ -249,11 +249,15 @@ def generate_launch_description():
         ]
     )
 
+    web_bridge = Node(
+        package='rosbridge_server',
+        node_name='rosbridge_server_node',
+        node_namespace='gui',
+        node_executable='rosbridge_websocket'
+    )
+
     web_files_root = os.path.join(os.environ["COLCON_PREFIX_PATH"], "avp_web_interface", "share", "avp_web_interface", "web")
     web_server = ExecuteProcess(cmd=["python3", "-m", "http.server", "8000"], cwd=web_files_root)
-    # TODO might change cmd when rosbridge_suite installed in ADE
-    rosbridge_root = os.path.join(os.environ["COLCON_PREFIX_PATH"], "rosbridge_server", "lib", "rosbridge_server")
-    web_bridge = ExecuteProcess(cmd=[os.path.join(rosbridge_root, "rosbridge_websocket")], log_cmd=True)
 
     return LaunchDescription([
         euclidean_cluster_param,
