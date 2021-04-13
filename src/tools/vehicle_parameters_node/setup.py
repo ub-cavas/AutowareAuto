843e3b701f2740f6f29715a7791f8b88e5b3666c
Esteve Fernandez <esteve.fernandez@tier4.jp>
2021-04-13 11:04:07 +0200

    [#400] Added central node for storing parameters for vehicle characteristics

---
+++ src/tools/vehicle_parameters_node/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+package_name = 'vehicle_parameters_node'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Esteve Fernandez',
+    author_email='esteve.fernandez@tier4.jp',
+    maintainer='Esteve Fernandez',
+    maintainer_email='esteve.fernandez@tier4.jp',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Node serving vehicle configuration parameters.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'vehicle_parameters_node = vehicle_parameters_node.server:main',
+        ],
+    },
+)
