f042fd66c8b8db5e483afeddef44a81f459abe20
Esteve Fernandez <esteve@apache.org>
2021-04-15 11:26:02 +0000

    [#115] Migrate documentation to readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+# hack for readthedocs to cause it to run doxygen first
+# https://github.com/rtfd/readthedocs.org/issues/388
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    subprocess.call('.doxygen/build.py', shell=True)
+
+master_doc = 'placeholder'
