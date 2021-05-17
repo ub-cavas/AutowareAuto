61d6592b6565eb06917b051126a9415b5f064ffc
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2021-05-17 12:32:55 +0200

    [#972] Move transform utils to the cpp file

--- src/common/state_estimation_nodes/include/state_estimation_nodes/transform_utils.hpp
+++ src/common/state_estimation_nodes/include/state_estimation_nodes/transform_utils.hpp
@@ -24,11 +24,13 @@
 
 
 #include <autoware_auto_msgs/msg/relative_position_with_covariance_stamped.hpp>
-#include <common/types.hpp>
+#include <state_estimation_nodes/visibility_control.hpp>
+
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
 #include <nav_msgs/msg/odometry.hpp>
-#include <tf2_eigen/tf2_eigen.h>
+
+#include <tf2/buffer_core.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 namespace autoware
@@ -53,83 +55,11 @@ namespace state_estimation
 ///
 /// @return     The transformation from source frame to the target frame.
 ///
-geometry_msgs::msg::TransformStamped get_tf__target__source(
+geometry_msgs::msg::TransformStamped STATE_ESTIMATION_NODES_PUBLIC get_tf__target__source(
   const tf2::BufferCore & buffer,
   const std_msgs::msg::Header::_frame_id_type & target_frame_id,
   const std_msgs::msg::Header::_frame_id_type & source_frame_id,
-  const std_msgs::msg::Header::_stamp_type & timestamp)
-{
-  // Get the transform between the msg and the output frame. We treat the
-  // possible exceptions as unrecoverable and let them bubble up.
-  return buffer.lookupTransform(
-    target_frame_id, source_frame_id, tf2_ros::fromMsg(timestamp));
-}
-
-template<typename ArrayT>
-Eigen::Matrix3d to_eigen(
-  const ArrayT & covariance,
-  const std::size_t start,
-  const std::size_t stride)
-{
-  return (Eigen::Matrix3d{} <<
-         covariance[start], covariance[start + stride], covariance[start + 2 * stride],
-         covariance[start + 1], covariance[start + stride + 1], covariance[start + 2 * stride + 1],
-         covariance[start + 2], covariance[start + stride + 2], covariance[start + 2 * stride + 2]).
-         finished();
-}
-
-template<typename ArrayT>
-void covariance_from_eigen(
-  ArrayT & covariance,
-  const Eigen::Matrix3d & matrix,
-  const std::size_t start,
-  const std::size_t stride)
-{
-  covariance[start] = matrix(0, 0);
-  covariance[start + 1] = matrix(1, 0);
-  covariance[start + 2] = matrix(2, 0);
-  covariance[start + stride] = matrix(0, 1);
-  covariance[start + stride + 1] = matrix(1, 1);
-  covariance[start + stride + 2] = matrix(2, 1);
-  covariance[start + 2 * stride] = matrix(0, 2);
-  covariance[start + 2 * stride + 1] = matrix(1, 2);
-  covariance[start + 2 * stride + 2] = matrix(2, 2);
-}
-
-///
-/// @brief      Rotate the covariance matrix
-///
-/// @param[in]  tf__new_child__old_child  The transform from the old child frame to the new one
-/// @param[in]  tf__new__old              The transform from the old parent frame to the new one
-/// @param[in]  covariance                The covariance to be rotated
-///
-/// @return     The rotated covariance matrix.
-///
-Eigen::Matrix3d rotate_covariance(
-  const Eigen::Isometry3d & tf__new_child__old_child,
-  const Eigen::Isometry3d & tf__new__old,
-  const Eigen::Matrix3d & covariance)
-{
-  return
-    tf__new_child__old_child.rotation().transpose() *
-    (tf__new__old.rotation() * covariance * tf__new__old.rotation().transpose()) *
-    tf__new_child__old_child.rotation();
-}
-
-///
-/// @brief      Rotate the covariance matrix of the odometry twist.
-///
-/// @param[in]  tf__new__old  The transform from the old frame to the new one.
-/// @param[in]  covariance                The covariance to be rotated.
-///
-/// @return     The rotated covariance matrix.
-///
-Eigen::Matrix3d rotate_covariance(
-  const Eigen::Isometry3d & tf__new__old,
-  const Eigen::Matrix3d & covariance)
-{
-  return tf__new__old.rotation() * covariance * tf__new__old.rotation().transpose();
-}
+  const std_msgs::msg::Header::_stamp_type & timestamp);
 
 ///
 /// @brief      Switch the frame_id and the child_frame_id of the message changing the message
@@ -143,7 +73,7 @@ Eigen::Matrix3d rotate_covariance(
 /// @return     The changed message with new frames.
 ///
 template<typename MsgT>
-MsgT switch_frames(
+MsgT STATE_ESTIMATION_NODES_PUBLIC switch_frames(
   const MsgT &,
   const std_msgs::msg::Header::_frame_id_type &,
   const std_msgs::msg::Header::_frame_id_type &,
@@ -163,7 +93,7 @@ MsgT switch_frames(
 /// @return     The changed message with new frames.
 ///
 template<typename MsgT>
-MsgT switch_frames(
+MsgT STATE_ESTIMATION_NODES_PUBLIC switch_frames(
   const MsgT &,
   const std_msgs::msg::Header::_frame_id_type &,
   const tf2::BufferCore &)
@@ -187,78 +117,11 @@ MsgT switch_frames(
 /// @return     A new odometry message with the requested frame_id and child_frame_id fields.
 ///
 template<>
-nav_msgs::msg::Odometry switch_frames(
+nav_msgs::msg::Odometry STATE_ESTIMATION_NODES_PUBLIC switch_frames(
   const nav_msgs::msg::Odometry & original_msg,
   const std_msgs::msg::Header::_frame_id_type & new_frame_id,
   const std_msgs::msg::Header::_frame_id_type & new_child_frame_id,
-  const tf2::BufferCore & buffer)
-{
-  if ((new_frame_id == original_msg.header.frame_id) &&
-    (new_child_frame_id == original_msg.child_frame_id))
-  {
-    return original_msg;
-  }
-  nav_msgs::msg::Odometry result_msg = original_msg;
-  const Eigen::Isometry3d tf__new_frame_id__msg_frame_id = tf2::transformToEigen(
-    get_tf__target__source(
-      buffer, new_frame_id, original_msg.header.frame_id, original_msg.header.stamp));
-  const Eigen::Isometry3d tf__new_child_frame_id__msg_child_frame_id = tf2::transformToEigen(
-    get_tf__target__source(
-      buffer, new_child_frame_id, original_msg.child_frame_id, original_msg.header.stamp));
-  // 1. Represent odom position and orientation as transformation tf__parent__child
-  Eigen::Isometry3d tf__msg_frame_id__msg_child_frame_id;
-  tf2::fromMsg(original_msg.pose.pose, tf__msg_frame_id__msg_child_frame_id);
-  // 2. Convert tf__parent__child to tf__new_parent__new_child as follows:
-  // tf__new_parent__new_child = tf__new_parent__parent * tf__parent__child * tf__child__new_child
-  const Eigen::Isometry3d tf__new_frame_id__new_child_frame_id =
-    tf__new_frame_id__msg_frame_id *
-    tf__msg_frame_id__msg_child_frame_id *
-    tf__new_child_frame_id__msg_child_frame_id.inverse();
-  // 3. Represent tf__new_parent__new_child as odom message again
-  result_msg.pose.pose = tf2::toMsg(tf__new_frame_id__new_child_frame_id);
-  // 4. Rotate the twist with the rotation part of tf__new_child__child
-  Eigen::Vector3d speed_in_child_frame;
-  tf2::fromMsg(original_msg.twist.twist.linear, speed_in_child_frame);
-  result_msg.twist.twist.linear = tf2::toMsg2(
-    tf__new_child_frame_id__msg_child_frame_id.rotation() * speed_in_child_frame);
-  Eigen::Vector3d angular_speed_in_child_frame;
-  tf2::fromMsg(original_msg.twist.twist.angular, angular_speed_in_child_frame);
-  result_msg.twist.twist.angular = tf2::toMsg2(
-    tf__new_child_frame_id__msg_child_frame_id.rotation() * angular_speed_in_child_frame);
-
-  const auto cov_stride = 6UL;
-  // Update position covariance
-  covariance_from_eigen(
-    result_msg.pose.covariance,
-    rotate_covariance(
-      tf__new_child_frame_id__msg_child_frame_id,
-      tf__new_frame_id__msg_frame_id,
-      to_eigen(original_msg.pose.covariance, 0UL, cov_stride)),
-    0UL, cov_stride);
-  // Update orientation covariance
-  covariance_from_eigen(
-    result_msg.pose.covariance,
-    rotate_covariance(
-      tf__new_child_frame_id__msg_child_frame_id,
-      tf__new_frame_id__msg_frame_id,
-      to_eigen(original_msg.pose.covariance, 21UL, cov_stride)),
-    21UL, cov_stride);
-  // Update linear speed covariance
-  covariance_from_eigen(
-    result_msg.twist.covariance,
-    rotate_covariance(
-      tf__new_child_frame_id__msg_child_frame_id,
-      to_eigen(original_msg.pose.covariance, 0UL, cov_stride)),
-    0UL, cov_stride);
-  // Update angular speed covariance
-  covariance_from_eigen(
-    result_msg.twist.covariance,
-    rotate_covariance(
-      tf__new_child_frame_id__msg_child_frame_id,
-      to_eigen(original_msg.pose.covariance, 21UL, cov_stride)),
-    21UL, cov_stride);
-  return result_msg;
-}
+  const tf2::BufferCore & buffer);
 
 ///
 /// @brief      Switch the frames for a relative position message.
@@ -271,49 +134,12 @@ nav_msgs::msg::Odometry switch_frames(
 /// @return     The changed relative position message with new frames
 ///
 template<>
-autoware_auto_msgs::msg::RelativePositionWithCovarianceStamped switch_frames(
+autoware_auto_msgs::msg::RelativePositionWithCovarianceStamped STATE_ESTIMATION_NODES_PUBLIC
+switch_frames(
   const autoware_auto_msgs::msg::RelativePositionWithCovarianceStamped & original_msg,
   const std_msgs::msg::Header::_frame_id_type & new_frame_id,
   const std_msgs::msg::Header::_frame_id_type & new_child_frame_id,
-  const tf2::BufferCore & buffer)
-{
-  if ((new_frame_id == original_msg.header.frame_id) &&
-    (new_child_frame_id == original_msg.child_frame_id))
-  {
-    return original_msg;
-  }
-  autoware_auto_msgs::msg::RelativePositionWithCovarianceStamped result_msg = original_msg;
-  const Eigen::Isometry3d tf__new_frame_id__msg_frame_id = tf2::transformToEigen(
-    get_tf__target__source(
-      buffer, new_frame_id, original_msg.header.frame_id, original_msg.header.stamp));
-  const Eigen::Isometry3d tf__new_child_frame_id__msg_child_frame_id = tf2::transformToEigen(
-    get_tf__target__source(
-      buffer, new_child_frame_id, original_msg.child_frame_id, original_msg.header.stamp));
-  // 1. Represent position as transformation tf__parent__child
-  Eigen::Vector3d translation;
-  tf2::fromMsg(original_msg.position, translation);
-  Eigen::Isometry3d tf__msg_frame_id__msg_child_frame_id{Eigen::Isometry3d::Identity()};
-  tf__msg_frame_id__msg_child_frame_id.translation() = translation;
-  // 2. Convert tf__parent__child to tf__new_parent__new_child as follows:
-  // tf__new_parent__new_child = tf__new_parent__parent * tf__parent__child * tf__child__new_child
-  const Eigen::Isometry3d tf__new_frame_id__new_child_frame_id =
-    tf__new_frame_id__msg_frame_id *
-    tf__msg_frame_id__msg_child_frame_id *
-    tf__new_child_frame_id__msg_child_frame_id.inverse();
-  // 3. Represent tf__new_parent__new_child as odom message again
-  result_msg.position = tf2::toMsg2(tf__new_frame_id__new_child_frame_id.translation());
-
-  const auto cov_stride = 3UL;
-  // Update position covariance
-  covariance_from_eigen(
-    result_msg.covariance,
-    rotate_covariance(
-      tf__new_child_frame_id__msg_child_frame_id,
-      tf__new_frame_id__msg_frame_id,
-      to_eigen(original_msg.covariance, 0UL, cov_stride)),
-    0UL, cov_stride);
-  return result_msg;
-}
+  const tf2::BufferCore & buffer);
 
 ///
 /// @brief      Switch the frame_id of the pose message.
@@ -327,40 +153,10 @@ autoware_auto_msgs::msg::RelativePositionWithCovarianceStamped switch_frames(
 /// @return     The changed message with its frame_id changed.
 ///
 template<>
-geometry_msgs::msg::PoseWithCovarianceStamped switch_frames(
+geometry_msgs::msg::PoseWithCovarianceStamped STATE_ESTIMATION_NODES_PUBLIC switch_frames(
   const geometry_msgs::msg::PoseWithCovarianceStamped & original_msg,
   const std_msgs::msg::Header::_frame_id_type & new_frame_id,
-  const tf2::BufferCore & buffer)
-{
-  if (new_frame_id == original_msg.header.frame_id) {
-    return original_msg;
-  }
-  geometry_msgs::msg::PoseWithCovarianceStamped result_msg = original_msg;
-  const Eigen::Isometry3d tf__new_frame_id__msg_frame_id = tf2::transformToEigen(
-    get_tf__target__source(
-      buffer, new_frame_id, original_msg.header.frame_id, original_msg.header.stamp));
-  Eigen::Isometry3d tf__msg_frame_id__msg_child_frame_id{};
-  tf2::fromMsg(original_msg.pose.pose, tf__msg_frame_id__msg_child_frame_id);
-  const Eigen::Isometry3d tf__new_frame_id__msg_child_frame_id =
-    tf__new_frame_id__msg_frame_id * tf__msg_frame_id__msg_child_frame_id;
-  result_msg.pose.pose = tf2::toMsg(tf__new_frame_id__msg_child_frame_id);
-
-  const auto cov_stride = 6UL;
-  // Update position covariance
-  covariance_from_eigen(
-    result_msg.pose.covariance,
-    rotate_covariance(
-      tf__new_frame_id__msg_frame_id, to_eigen(original_msg.pose.covariance, 0UL, cov_stride)),
-    0UL, cov_stride);
-  // Update orientation covariance
-  covariance_from_eigen(
-    result_msg.pose.covariance,
-    rotate_covariance(
-      tf__new_frame_id__msg_frame_id, to_eigen(original_msg.pose.covariance, 21UL, cov_stride)),
-    21UL, cov_stride);
-  return result_msg;
-}
-
+  const tf2::BufferCore & buffer);
 
 ///
 /// @brief      Switch the frame_id of the twist message.
@@ -374,40 +170,10 @@ geometry_msgs::msg::PoseWithCovarianceStamped switch_frames(
 /// @return     The changed message with its frame_id changed.
 ///
 template<>
-geometry_msgs::msg::TwistWithCovarianceStamped switch_frames(
+geometry_msgs::msg::TwistWithCovarianceStamped STATE_ESTIMATION_NODES_PUBLIC switch_frames(
   const geometry_msgs::msg::TwistWithCovarianceStamped & original_msg,
   const std_msgs::msg::Header::_frame_id_type & new_frame_id,
-  const tf2::BufferCore & buffer)
-{
-  if (new_frame_id == original_msg.header.frame_id) {
-    return original_msg;
-  }
-  geometry_msgs::msg::TwistWithCovarianceStamped result_msg = original_msg;
-  const Eigen::Isometry3d tf__new_frame_id__msg_frame_id = tf2::transformToEigen(
-    get_tf__target__source(
-      buffer, new_frame_id, original_msg.header.frame_id, original_msg.header.stamp));
-  Eigen::Vector3d angular_speed_in_child_frame;
-  tf2::fromMsg(original_msg.twist.twist.angular, angular_speed_in_child_frame);
-  result_msg.twist.twist.angular = tf2::toMsg2(
-    tf__new_frame_id__msg_frame_id.rotation() * angular_speed_in_child_frame);
-
-  const auto cov_stride = 6UL;
-  // Update linear speed covariance
-  covariance_from_eigen(
-    result_msg.twist.covariance,
-    rotate_covariance(
-      tf__new_frame_id__msg_frame_id,
-      to_eigen(original_msg.twist.covariance, 0UL, cov_stride)),
-    0UL, cov_stride);
-  // Update angular speed covariance
-  covariance_from_eigen(
-    result_msg.twist.covariance,
-    rotate_covariance(
-      tf__new_frame_id__msg_frame_id,
-      to_eigen(original_msg.twist.covariance, 21UL, cov_stride)),
-    21UL, cov_stride);
-  return result_msg;
-}
+  const tf2::BufferCore & buffer);
 
 
 }  // namespace state_estimation
