385772999f9a28df792f1167ceaec8ec89bc9b90
Esteve Fernandez <esteve.fernandez@tier4.jp>
2021-08-20 15:09:29 +0200

    Check package.xml for package names

--- tools/list_changed_packages.sh
+++ tools/list_changed_packages.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-all_pkgs=`colcon list`
-all_modified_files=`git diff --name-only origin/master`
+all_modified_files=$(git diff --name-only origin/master)
 all_modified_pkgs=()
-while IFS= read -r file; do
-	echo $file
-	echo
-	while IFS= read -r pkg; do
-		pkg_path=$(echo ${pkg} | awk '{print $2}')
-		match=`echo $file | grep $pkg_path`
-		if [ ! -z ${match} ]; then
-			pkg_name=$(echo ${pkg} | awk '{print $1}')
-			all_modified_pkgs+=( $pkg_name )
+for modified_file in ${all_modified_files}; do
+	dname=$(dirname ${modified_file})
+	while [ "${dname}" != "." ]; do
+		if [ -f ${dname}/package.xml ]; then
+			pkg_name=$(grep name "${dname}/package.xml")
+			all_modified_pkgs+=($(echo ${pkg_name}|cut -d'>' -f2|cut -d'<' -f1))
+			break;
 		fi
-	done < <(printf '%s\n' "$all_pkgs")
-done < <(printf '%s\n' "$all_modified_files")
+		dname=$(dirname ${dname})
+	done
+done
+
+for pkg in "${all_modified_pkgs[@]}"; do
+	echo $pkg
+done
