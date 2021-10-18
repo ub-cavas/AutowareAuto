f7513541102ef962dbb052b0ea9a3e8352bc32ee
wep21 <border_goldenmarket@yahoo.co.jp>
2021-10-18 12:31:01 +0900

    Fix build error for pure pursuit nodes
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- src/control/pure_pursuit_nodes/test/src/pure_pursuit_integration_test.cpp
+++ src/control/pure_pursuit_nodes/test/src/pure_pursuit_integration_test.cpp
@@ -158,8 +158,8 @@ public:
   {
     using namespace std::string_literals;
     RCLCPP_INFO(get_logger(), "MotionControllerPubSub checking...");
-    RCLCPP_INFO(get_logger(), "\tShould receive: "s + std::to_string(m_num_msgs * 2U));
-    RCLCPP_INFO(get_logger(), "\tActual received: "s + std::to_string(m_num_received_msgs));
+    RCLCPP_INFO_STREAM(get_logger(), "\tShould receive: "s << std::to_string(m_num_msgs * 2U));
+    RCLCPP_INFO_STREAM(get_logger(), "\tActual received: "s << std::to_string(m_num_received_msgs));
     // Relatively loose check: make sure you got at least something
     bool8_t message_received = m_num_received_msgs > 0U;
     // Make sure you're close enough
@@ -167,10 +167,10 @@ public:
     // Definitely shouldn't get too many
     message_received = message_received && (m_num_received_msgs <= m_num_msgs * 2U);
 
-    RCLCPP_INFO(get_logger(), "\tShould have correct answer: "s + std::to_string(m_num_msgs));
-    RCLCPP_INFO(
+    RCLCPP_INFO_STREAM(get_logger(), "\tShould have correct answer: "s << std::to_string(m_num_msgs));
+    RCLCPP_INFO_STREAM(
       get_logger(),
-      "\tActual have correct answer: "s + std::to_string(m_num_correct_output));
+      "\tActual have correct answer: "s << std::to_string(m_num_correct_output));
     bool8_t is_correct = m_num_correct_output <= m_num_msgs;
     is_correct = is_correct && ((m_num_correct_output * 2U) > (m_num_msgs * 7 / 10));
     return message_received && is_correct;
