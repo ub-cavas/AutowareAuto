eb7125035dd71cadda1b6c9b737d67ea911fd215
wep21 <border_goldenmarket@yahoo.co.jp>
2021-12-02 14:48:36 +0900

    Fix build error for spinnaker_camera_nodes
    Signed-off-by: wep21 <border_goldenmarket@yahoo.co.jp>

--- src/drivers/spinnaker_camera_nodes/src/spinnaker_camera_node.cpp
+++ src/drivers/spinnaker_camera_nodes/src/spinnaker_camera_node.cpp
@@ -96,11 +96,11 @@ spinnaker::CameraListWrapper & SpinnakerCameraNode::create_cameras_from_params(
           prefix_dot + "device_link_throughput_limit", kDefaultDeviceThroughputLimit)};
       return spinnaker::CameraSettings{
       static_cast<std::uint32_t>(
-        declare_parameter(prefix_dot + "window_width").get<std::uint64_t>()),
+        declare_parameter<int64_t>(prefix_dot + "window_width")),
       static_cast<std::uint32_t>(
-        declare_parameter(prefix_dot + "window_height").get<std::uint64_t>()),
-      declare_parameter(prefix_dot + "fps").template get<float64_t>(),
-      declare_parameter(prefix_dot + "pixel_format").template get<std::string>(),
+        declare_parameter<int64_t>(prefix_dot + "window_height")),
+      declare_parameter<float64_t>(prefix_dot + "fps"),
+      declare_parameter<std::string>(prefix_dot + "pixel_format"),
       camera_frame_id_param,
       camera_serial_number_param,
       device_link_throughput_limit_param};
