8c8dcd7c2851a83d32d7a270dfd9840c5c598d4b
Esteve Fernandez <esteve.fernandez@tier4.jp>
2022-01-10 16:18:07 +0100

    Revert exit codes changes

--- src/tools/autoware_testing/autoware_testing/smoke_test.py
+++ src/tools/autoware_testing/autoware_testing/smoke_test.py
@@ -85,5 +85,5 @@ def generate_test_description():
 class TestProcessOutput(unittest.TestCase):
 
     def test_exit_code(self, proc_output, proc_info):
-        # Check that process exits with code -2 code: termination request, sent to the program
-        launch_testing.asserts.assertExitCodes(proc_info, [-2])
+        # Check that process exits with code -15 code: termination request, sent to the program
+        launch_testing.asserts.assertExitCodes(proc_info, [-15])
