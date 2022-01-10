445dc8cb21849d4a9a31da582f96c0cff1663a6c
Esteve Fernandez <esteve.fernandez@tier4.jp>
2022-01-10 16:18:07 +0100

    Revert exit codes changes

--- src/tools/benchmark_tool_nodes/test/benchmark_tool_node_launch.test.py
+++ src/tools/benchmark_tool_nodes/test/benchmark_tool_node_launch.test.py
@@ -68,5 +68,5 @@ def generate_test_description():
 class TestProcessOutput(unittest.TestCase):
 
     def test_exit_code(self, proc_output, proc_info, benchmark_tool):
-        # Check that process exits with code -2 code: termination request, sent to the program
-        launch_testing.asserts.assertExitCodes(proc_info, [-2], process=benchmark_tool)
+        # Check that process exits with code -15 code: termination request, sent to the program
+        launch_testing.asserts.assertExitCodes(proc_info, [-15], process=benchmark_tool)
