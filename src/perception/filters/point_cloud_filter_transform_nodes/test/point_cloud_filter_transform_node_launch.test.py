19b5918993b0326885f0a0977697100ad1686d5d
Esteve Fernandez <esteve.fernandez@tier4.jp>
2022-01-11 12:46:23 +0100

    Update exit code

--- src/perception/filters/point_cloud_filter_transform_nodes/test/point_cloud_filter_transform_node_launch.test.py
+++ src/perception/filters/point_cloud_filter_transform_nodes/test/point_cloud_filter_transform_node_launch.test.py
@@ -53,9 +53,9 @@ def generate_test_description():
 class TestProcessOutput(unittest.TestCase):
 
     def test_exit_code(self, proc_output, proc_info, point_cloud_filter_transform_node):
-        # Check that process exits with code -15 code: termination request, sent to the program
+        # Check that process exits with code -2 code: termination request, sent to the program
         launch_testing.asserts.assertExitCodes(
             proc_info,
-            [-15],
+            [-2],
             process=point_cloud_filter_transform_node
         )
