dd26ca3fe7bfd5810fd4512caea5e816c9c7412d
Esteve Fernandez <esteve.fernandez@tier4.jp>
2022-01-11 12:50:09 +0100

    Expect -2 insted of -15 as exit code

--- src/localization/ndt_nodes/test/ndt_map_publisher_launch.test.py
+++ src/localization/ndt_nodes/test/ndt_map_publisher_launch.test.py
@@ -63,5 +63,5 @@ def generate_test_description():
 class TestProcessOutput(unittest.TestCase):
 
     def test_exit_code(self, proc_output, proc_info, map_provider_node_runner):
-        # Check that process exits with code -15 code: termination request, sent to the program
-        launch_testing.asserts.assertExitCodes(proc_info, [-15], process=map_provider_node_runner)
+        # Check that process exits with code -2 code: termination request, sent to the program
+        launch_testing.asserts.assertExitCodes(proc_info, [-2], process=map_provider_node_runner)
