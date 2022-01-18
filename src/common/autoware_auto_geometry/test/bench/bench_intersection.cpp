7afe3f05bb78054227f22c513ed19ac67f749d14
beyza <bnk@leodrive.ai>
2022-01-18 17:06:13 +0300

    update benchmark

--- src/common/autoware_auto_geometry/test/bench/bench_intersection.cpp
+++ src/common/autoware_auto_geometry/test/bench/bench_intersection.cpp
@@ -8,18 +8,27 @@
 #include <geometry/intersection.hpp>
 #include <benchmark/benchmark.h>
 
+
 namespace bg = boost::geometry;
-typedef bg::model::d2::point_xy<double> point_type;
-typedef bg::model::polygon<point_type> polygon_type;
+typedef bg::model::point<autoware::common::types::float32_t, 2, bg::cs::cartesian> point_type;
+typedef bg::model::ring<point_type> polygon_type;
+
+using Point = geometry_msgs::msg::Point32;
+
+struct TestPoint
+{
+    autoware::common::types::float32_t x;
+    autoware::common::types::float32_t y;
+
+};
 
 static void bench_convex_polygon_intersection_2d(benchmark::State& state) {
 
-    std::list<point_type> polygon1{{ 6, 1},{ 6, 8},{ 2, 8},{-2, 4},{ 1, 1},{ 6, 1}};
-    std::list<point_type> polygon2{{1, 2},{2, 6},{4, 2},{1, 2}};
+    std::list<TestPoint> polygon1{{6, 1},{ 6, 8},{ 2, 8},{-2, 4},{ 1, 1},{ 6, 1}};
+    std::list<TestPoint> polygon2{{1, 3},{2, 6},{4, 2},{1, 3}};
 
     for (auto _ : state)
         autoware::common::geometry::convex_polygon_intersection2d(polygon1, polygon2);
-
 }
 
 // Register the function as a benchmark
