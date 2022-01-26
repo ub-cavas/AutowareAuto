36754991d967c5bb59bf1cae635744d4411a198f
Esteve Fernandez <esteve.fernandez@tier4.jp>
2022-01-26 16:35:48 +0100

    Update tests to also accept -6

--- src/mapping/had_map/lanelet2_map_provider/test/lanelet2_map_provider_launch.test.py
+++ src/mapping/had_map/lanelet2_map_provider/test/lanelet2_map_provider_launch.test.py
@@ -60,5 +60,5 @@ def generate_test_description():
 class TestProcessOutput(unittest.TestCase):
 
     def test_exit_code(self, proc_output, proc_info, lanelet2_map_provider):
-        # Check that process exits with code -2 code: termination request, sent to the program
-        launch_testing.asserts.assertExitCodes(proc_info, [-2], process=lanelet2_map_provider)
+        # Check that process exits with expected codes: either SIGINT or SIGTERM codes are fine
+        launch_testing.asserts.assertExitCodes(proc_info, [-2, -6, -15], process=lanelet2_map_provider)
