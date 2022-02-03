bea4b8e05b1766ca9550b613fecf7030ec8e5b38
Esteve Fernandez <esteve.fernandez@tier4.jp>
2022-02-03 15:59:43 +0100

    Added fine-grained error ignores

--- src/planning/behavior_planner_nodes/include/behavior_planner_nodes/behavior_planner_node.hpp
+++ src/planning/behavior_planner_nodes/include/behavior_planner_nodes/behavior_planner_node.hpp
@@ -54,7 +54,14 @@
 #include <lanelet2_core/LaneletMap.h>
 #pragma GCC diagnostic pop
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wdouble-promotion"
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wuseless-cast"
 #include <tf2_ros/transform_listener.h>
+#pragma GCC diagnostic pop
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wconversion"
