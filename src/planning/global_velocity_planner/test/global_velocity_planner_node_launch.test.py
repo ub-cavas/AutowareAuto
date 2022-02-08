2f67e57769cb720913443231709d3c0581e490bf
Esteve Fernandez <esteve.fernandez@tier4.jp>
2022-02-08 14:04:27 +0100

    Update exit code

--- src/planning/global_velocity_planner/test/global_velocity_planner_node_launch.test.py
+++ src/planning/global_velocity_planner/test/global_velocity_planner_node_launch.test.py
@@ -24,6 +24,8 @@ import os
 import pytest
 import unittest
 
+import signal
+
 
 @pytest.mark.launch_test
 def generate_test_description():
@@ -51,9 +53,8 @@ def generate_test_description():
 class TestProcessOutput(unittest.TestCase):
 
     def test_exit_code(self, proc_output, proc_info, global_velocity_planner):
-        # Check that process exits with code -15 code: termination request, sent to the program
+        # Check that process exits with expected codes: either SIGINT, SIGABRT or SIGTERM codes
+        # are fine
         launch_testing.asserts.assertExitCodes(
-            proc_info,
-            [-15],
-            process=global_velocity_planner
-        )
+            proc_info, [-signal.SIGINT, -signal.SIGABRT, -signal.SIGTERM],
+            process=global_velocity_planner)
