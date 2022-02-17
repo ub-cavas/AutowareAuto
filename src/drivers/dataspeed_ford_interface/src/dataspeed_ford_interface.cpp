467a7b4393d05d4180aa7ca60c097230072cdfcf
Shumin326 <shumin@seas.upenn.edu>
2022-02-16 22:31:57 -0500

    Revert "remove the same code block from handle_mode_request"
    This reverts commit d0e6c3dff8ad9d1a6063f08a4febe42132474f70.

--- src/drivers/dataspeed_ford_interface/src/dataspeed_ford_interface.cpp
+++ src/drivers/dataspeed_ford_interface/src/dataspeed_ford_interface.cpp
@@ -335,7 +335,10 @@ bool8_t DataspeedFordInterface::handle_mode_change_request(ModeChangeRequest::Sh
   // Request MANUAL -> AUTONOMOUS
   else if (request->mode == ModeChangeRequest::MODE_AUTONOMOUS) {
     m_dbw_state_machine->user_request(true);
-    if (!m_dbw_state_machine->enabled()) {
+    if (m_dbw_state_machine->enabled()) {
+      m_dbw_enable_cmd_pub->publish(send_req);
+    } else {
+      m_dbw_disable_cmd_pub->publish(send_req);
       RCLCPP_ERROR_THROTTLE(
         m_logger, m_clock, CLOCK_1_SEC,
         "Mode change request failed: state machine is not enabled.");
