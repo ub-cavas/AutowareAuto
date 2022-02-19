ce5df69bd8cb29f2584863be24540f96ca0dc7c1
Steven Korzelius <1235477+smk32@users.noreply.github.com>
2022-02-19 12:13:11 -0500

    Start of change to using non VSC turn indicators

--- src/drivers/dataspeed_ford_interface/src/dataspeed_ford_interface.cpp
+++ src/drivers/dataspeed_ford_interface/src/dataspeed_ford_interface.cpp
@@ -199,31 +199,9 @@ bool8_t DataspeedFordInterface::send_state_command(const VehicleStateCommand & m
 
   // TODO everything else should go in their own callbacks
   // deprecated in this function
-  // only handle hear and handbrakes
+  // only handle gear and handbrakes
 
-  switch (msg.blinker) {
-    case VehicleStateCommand::BLINKER_NO_COMMAND:
-      // Keep previous
-      break;
-    case VehicleStateCommand::BLINKER_OFF:
-      m_misc_cmd.cmd.value = TurnSignal::NONE;
-      break;
-    case VehicleStateCommand::BLINKER_LEFT:
-      m_misc_cmd.cmd.value = TurnSignal::LEFT;
-      break;
-    case VehicleStateCommand::BLINKER_RIGHT:
-      m_misc_cmd.cmd.value = TurnSignal::RIGHT;
-      break;
-    case VehicleStateCommand::BLINKER_HAZARD:
-      m_misc_cmd.cmd.value = TurnSignal::HAZARD;
-      break;
-    default:
-      m_misc_cmd.cmd.value = TurnSignal::NONE;
-      RCLCPP_ERROR_THROTTLE(
-        m_logger, m_clock, CLOCK_1_SEC,
-        "Received command for invalid turn signal state.");
-      ret = false;
-      break;
+
   }
   m_misc_cmd.header.stamp = msg.stamp;
   m_seen_vehicle_state_cmd = true;
@@ -375,6 +353,33 @@ void DataspeedFordInterface::send_horn_command(const HornCommand & msg)
   RCLCPP_ERROR_THROTTLE(
     m_logger, m_clock, CLOCK_1_SEC,
     "Dataspeed Ford interface does not support sending horn command.");
+}
+
+void DataspeedFordInterface::send_turn_indicators_command(const TurnIndicatorsCommand & msg)
+{
+  switch (msg.blinker) {
+    case TurnIndicatorsCommand::BLINKER_NO_COMMAND:
+      // Keep previous
+      break;
+    case VehicleStateCommand::BLINKER_OFF:
+      m_misc_cmd.cmd.value = TurnSignal::NONE;
+      break;
+    case VehicleStateCommand::BLINKER_LEFT:
+      m_misc_cmd.cmd.value = TurnSignal::LEFT;
+      break;
+    case VehicleStateCommand::BLINKER_RIGHT:
+      m_misc_cmd.cmd.value = TurnSignal::RIGHT;
+      break;
+    case VehicleStateCommand::BLINKER_HAZARD:
+      m_misc_cmd.cmd.value = TurnSignal::HAZARD;
+      break;
+    default:
+      m_misc_cmd.cmd.value = TurnSignal::NONE;
+      RCLCPP_ERROR_THROTTLE(
+        m_logger, m_clock, CLOCK_1_SEC,
+        "Received command for invalid turn signal state.");
+      ret = false;
+      break;
 }
 
 void DataspeedFordInterface::send_wipers_command(const WipersCommand & msg)
